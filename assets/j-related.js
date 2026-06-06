34f9bcb34388504ab6a2b20527e373ad36fd90b8
Mohamed Sameh <mxsameh@gmail.com>
2026-06-06 15:32:50 +0300

    s

--- assets/j-related.js
+++ assets/j-related.js
@@ -1,8 +1,19 @@
 var swiper = new Swiper(".s-rel .swiper", {
-  slidesPerView: 1.34,
-  spaceBetween: "20rem",
+  slidesPerView: 3,
+  spaceBetween: "44rem",
   navigation: {
     nextEl: ".s-rel #swiper-b-nxt",
     prevEl: ".s-rel #swiper-b-prv",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1.34,
+      spaceBetween: "20rem",
+    },
+
+    770: {
+      slidesPerView: 3,
+      spaceBetween: 44,
+    },
   },
 });
